a34d2260e68a582af2e0eb14b3666f4da5132478
TristanGonzalezMesa <56074287+TristanGonzalezMesa@users.noreply.github.com>
2020-07-13 14:51:21 +0200

    Session logged
    Session logged in, niet ingelogd direct naar index

--- Bestellingen.php
+++ Bestellingen.php
@@ -1,6 +1,7 @@
 <?php include ('Database.php'); 
 session_start();
 ?>
+<?php if(isset($_SESSION['Email']) == "Email"){echo "";}else{header('Location: ./index.php');}?>
 <?php if(isset($_SESSION['Email']) == "Email"){echo "<li><a href='./uitloggen.php'>Uitloggen</a></li><li><a href='Kopen.php'>Volgers Kopen</a></li><li><a href='./Bestellingen.php'>Bestellingen</a></li>";}else{echo "<li><a href='Login.php'>Inloggen</a></li>";}?>
 <?php
 if(isset($_SESSION["Email"]) == "Email"){
--- index.php
+++ index.php
@@ -3,3 +3,5 @@ session_start();
 ?>
 
 <?php if(isset($_SESSION['Email']) == "Email"){echo "<li><a href='./uitloggen.php'>Uitloggen</a></li><li><a href='Kopen.php'>Volgers Kopen</a></li><li><a href='./Bestellingen.php'>Bestellingen</a></li>";}else{echo "<li><a href='Login.php'>Inloggen</a></li>";}?>
+
+<h1> Volgers kopen? </h1>
