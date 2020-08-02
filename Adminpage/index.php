d3f281faee00adf8d2d90bfeaa00dcf987909bd2
TristanGonzalezMesa <56074287+TristanGonzalezMesa@users.noreply.github.com>
2020-08-02 10:53:05 +0200

    s
    s

--- Adminpage/index.php
+++ Adminpage/index.php
@@ -1,3 +1,11 @@
+<?php include ('../Database.php'); 
+session_start();
+?>
 <p> Adminpage </p>
 
 <li><a href='../Adminpage/Bestellingen.php'>Bestellingen</a></li>
+
+
+<?php if(isset($_SESSION['rol']) == "9"){echo "";}else{header('Location: ../index.php');}?>
+<?php if(isset($_SESSION['Email']) == "Email"){echo "<li><a href='../uitloggen.php'>Uitloggen</a></li>";}else{echo "<li><a href='Login.php'>Inloggen</a></li>";}?>
+<?php if(isset($_SESSION['rol'])){if ($_SESSION['rol'] == 9) {echo "<li><a href='../Adminpage/index.php'>CMS</a></li>";}}?>
